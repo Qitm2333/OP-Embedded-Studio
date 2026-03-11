8b2002d5b944177b56df4596a0c7dd2b15c20f93
Anton A S <eddclyde@yandex.ru>
2026-03-11 17:36:39 +0300

    Fix icon vector positioning: zero x/y after appendChild

--- packages/core/src/tools/create.ts
+++ packages/core/src/tools/create.ts
@@ -259,6 +259,8 @@ function createIconFrame(
     vector.name = 'path'
     vector.resize(size, size)
     frame.appendChild(vector)
+    vector.x = 0
+    vector.y = 0
 
     figma.graph.updateNode(vector.id, {
       vectorNetwork: path.vectorNetwork
